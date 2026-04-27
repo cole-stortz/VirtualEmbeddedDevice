0064e7d6681c72c84cb22ddf96f03a18ec233a14
cdstortz <cdstortz@gmail.com>
2026-04-27 16:28:19 -0700

    Modified the threading example

--- testfiles/threading.py
+++ testfiles/threading.py
@@ -1,11 +1,18 @@
 import threading
+import socket
+
+HOST = 'localhost'
+PORT = 12345
+
+def definition_of_something():
+    pass
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         while True:
-            conn, addr = s.accept()``
-            thread = threading.Thread(target=message_handler, args=(conn, addr))
+            conn, addr = s.accept()
+            thread = threading.Thread(target=definition_of_something, args=(conn, addr))
             thread.start()
 
