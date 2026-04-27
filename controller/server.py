72eeaa69672a24e8d193591a772b5224ce125017
cdstortz <cdstortz@gmail.com>
2026-04-27 10:55:01 -0600

    added header files to controller server and device

--- controller/server.py
+++ controller/server.py
@@ -1,3 +1,16 @@
+'''
+This Program is a TCP server that connects to a device client to send and recieve data and commands. There is an UI that is adaptive to 
+the embedded device config and will add according buttons and options to the UI. There will also be a log or output terminal to show the
+data send and recieved from the device.
+
+Device elements:
+- LEDs: Simulate status indicators or visual feedback.
+- Buttons: Simulate user input or control interfaces.
+- Sensors: Simulate data collection from the environment (e.g., temperature, humidity).
+    - Temprature, Light, Distance
+- Motors: Simulate actuators for movement or mechanical actions.
+
+'''
 import socket
 
 HOST = 'localhost'
---
+++ device/src/device.cpp
@@ -0,0 +1,14 @@
+/*
+This file runs the Virtual Device which simulates an embedded system. It listens for incomming connections from the server and responds to commands send by the server. 
+
+Embedded Device Addons:
+- LEDs: Simulate status indicators or visual feedback.
+- Buttons: Simulate user input or control interfaces.
+- Sensors: Simulate data collection from the environment (e.g., temperature, humidity).
+    - Temprature, Light, Distance
+- Motors: Simulate actuators for movement or mechanical actions.
+
+Embedded Device Communication:
+- The device listens for incoming connections from the server via TCP/IP.
+- It processes commands sent by the server and responds accordingly.
+*/
